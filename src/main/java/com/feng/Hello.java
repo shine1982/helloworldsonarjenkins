a177a22fb10d18221fc4a946dcca634860f69b0f
Feng QIN <qinfeng0152@gmail.com>
2018-11-04 16:36:10 +0100

    hello3

--- src/main/java/com/feng/Hello.java
+++ src/main/java/com/feng/Hello.java
@@ -16,4 +16,18 @@ public class Hello {
         return "hello world";
     }
 
+    public String tototitit(String name){
+        if (name != null)
+            return "hello "+ name;
+
+        return "hello world";
+    }
+
+    public String tototitit2(String name){
+        if (name != null)
+            return "hello "+ name;
+
+        return "hello world";
+    }
+
 }
