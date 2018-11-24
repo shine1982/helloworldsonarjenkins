82e7072a555e558698c730c7ed7e6040fefbba98
Feng QIN <qinfeng0152@gmail.com>
2018-11-24 19:38:20 +0100

    add new branch

---
+++ src/main/java/com/feng/Hello3.java
@@ -0,0 +1,20 @@
+package com.feng;
+
+public class Hello3 {
+
+    public String sayHi(String name){
+        if (name != null)
+            return "hello "+ name;
+
+        return "hello world";
+    }
+
+    public String sayHi2(String name){
+        if (name != null)
+            return "hello "+ name;
+
+        return "hello world";
+    }
+
+
+}
